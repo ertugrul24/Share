0b970d7aaafad7355534a5db4e2b7b2b183a7ceb
Ertugrul <ertugrul.gurbuz@student.hu.nl>
2017-09-17 00:18:50 +0200

    test

--- Les 2/Exercise 2_4.py
+++ Les 2/Exercise 2_4.py
@@ -3,3 +3,4 @@ urengewerkt = int(input('Hoeveel uren heb je gewerkt: '))
 salaris = uurloon * urengewerkt
 antwoord = str (urengewerkt) + ' ' + 'uur werken levert' + ' '+str(salaris) + ' ' + 'euro op'
 print (antwoord)
+fd
